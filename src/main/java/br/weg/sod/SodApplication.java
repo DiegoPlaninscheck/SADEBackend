2fcfc7205c63f70bef8f5e7e27d4f35fd1bdc6cb
Diego <92130153+DiegoPlaninscheck@users.noreply.github.com>
2023-05-23 21:45:23 -0300

    feat: tentando fazer email

--- src/main/java/br/weg/sod/SodApplication.java
+++ src/main/java/br/weg/sod/SodApplication.java
@@ -1,13 +1,25 @@
 package br.weg.sod;
 
+import br.weg.sod.model.service.EmailSenderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class SodApplication {
 
+	@Autowired
+	EmailSenderService emailSenderService;
+
 	public static void main(String[] args) {
 		SpringApplication.run(SodApplication.class, args);
+	}
+
+	@EventListener(ApplicationEvent.class)
+	public void sendEmail(){
+		emailSenderService.sendEmail("diego_planinscheck@estudante.sc.senai.br", "Subject", "Mensagem aqui!");
 	}
 
 }
